13f24f1fcf4b41d2eea7af9723ee0793e821bfc8
Geovana Souza <geovana21072003@gmail.com>
2023-02-13 14:15:51 -0300

    basico de cartas e jogador

---
+++ facebook_2/src/negocio/Jogador.java
@@ -0,0 +1,26 @@
+package negocio;
+
+public class Jogador {
+	public String nome;
+	public int hp;
+	public int semente;
+	
+	public Jogador() {
+		
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	
+
+}

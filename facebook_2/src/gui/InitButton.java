1c1fbdea1b910c07704143907fe32787dc270832
Cauã Braga de Lima <cauabrgal11@gmail.com>
2023-02-17 14:41:02 -0300

    main screen

---
+++ facebook_2/src/gui/InitButton.java
@@ -0,0 +1,39 @@
+package gui;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import javax.swing.JButton;
+
+public class InitButton extends JButton implements Action
+{
+    String name;
+
+    public JButton getButton(String name)
+    {
+        this.name = name;
+        JButton button = new JButton();
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setName(name);
+        button.setVisible(true);
+        return button;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        
+    }
+
+    @Override
+    public Object getValue(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void putValue(String key, Object value) {
+        // TODO Auto-generated method stub
+        
+    }
+}
--- facebook_2/src/gui/MainWindow.java
+++ facebook_2/src/gui/MainWindow.java
@@ -1,13 +1,27 @@
 package gui;
+import java.awt.Color;
+import java.awt.LayoutManager;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainWindow extends JFrame
 {
     public MainWindow()
     {
         this.setSize(300, 600);
-        this.setVisible(true);
+        this.setTitle("Project Facebook 2");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.getPanel();
+        this.setVisible(true);
+        
     }
+
+    private void getPanel() 
+    {
+        this.setLayout(new BorderLayout());
+        panel.add(new InitButton().getButton("COMEÇAR!"));
+    }
+
+
 }

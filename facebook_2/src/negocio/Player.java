9add2b754871a283a366da020151329140b59255
Geovana Souza <geovana21072003@gmail.com>
2023-02-20 18:01:26 -0300

    cartas e player

---
+++ facebook_2/src/negocio/Player.java
@@ -0,0 +1,24 @@
+package negocio;
+
+public class Player {
+	public String name;
+	public int hp;
+	public int seed;
+
+	public Player() {
+
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHp() {
+		return hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+
+}

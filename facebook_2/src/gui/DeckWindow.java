e445ac0c08aca3a0bcd773c7c91fd9554da04694
Geovana Souza <geovana21072003@gmail.com>
2023-02-20 17:41:31 -0300

    botao init chama deckWindow

--- facebook_2/src/gui/DeckWindow.java
+++ facebook_2/src/gui/DeckWindow.java
@@ -1,41 +1,26 @@
 package gui;
 
-<<<<<<< HEAD
 import java.awt.Color;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
-public class DeckWindow extends JFrame{
-	public DeckWindow()
-    {
-        this.setSize(600, 300);
-=======
-import javax.swing.JFrame;
 
 public class DeckWindow extends JFrame
 {
     public DeckWindow()
     {
-        this.setSize(300, 600);
->>>>>>> db53aa4009b7f7ae6f886979be6206d1619b0c64
+        this.setSize(600, 300);
         this.setTitle("Project Facebook 2");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.getPanel();
         this.setVisible(true);
-        
     }
-
+    
     private void getPanel() 
     {
-<<<<<<< HEAD
     	JPanel panel = new JPanel();
     	panel.setBackground(Color.darkGray);
     	this.add(panel);
     }
 
-=======
     
-    }
->>>>>>> db53aa4009b7f7ae6f886979be6206d1619b0c64
-}
+ }
--- facebook_2/src/gui/Main.java
+++ facebook_2/src/gui/Main.java
@@ -1,19 +1,9 @@
 package gui;
 
-<<<<<<< HEAD
 public class Main {
 
 	public static void main(String[] args) {
         MainWindow window = new MainWindow();
 
 	}
-
-=======
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-        MainWindow window = new MainWindow();
-    }
->>>>>>> db53aa4009b7f7ae6f886979be6206d1619b0c64
 }
